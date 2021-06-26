1188807470290e612e0faed7257bb2c8ef01e671
Eric <ericsu0630@gmail.com>
2021-06-26 23:19:16 +0800

    wrapped form in an iframe and gave it a height

--- lib/widgets/rsvp_form.dart
+++ lib/widgets/rsvp_form.dart
@@ -3,31 +3,30 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
 
-import 'package:webviewx/webviewx.dart';
-
 class RsvpForm extends StatefulWidget {
   @override
   _RsvpFormState createState() => _RsvpFormState();
 }
 
 class _RsvpFormState extends State<RsvpForm> {
-  late WebViewXController webviewController;
   @override
   void initState() {
     super.initState();
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+        'my_google_form',
+        (int viewId) => IFrameElement()
+          ..src =
+              'https://docs.google.com/forms/d/e/1FAIpQLScRSiBezvevmvAWelQLAROmCD73PUY28NFeuD_CKKfkbkMVHQ/viewform?usp=sf_link' //@Tsu URL for google form goes here
+          ..style.border = 'none');
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height - 64,
-      width: MediaQuery.of(context).size.width * 0.8,
-      color: Colors.white,
-      child: WebViewX(
-        initialContent: 'https://docs.google.com/forms/d/e/1FAIpQLScRSiBezvevmvAWelQLAROmCD73PUY28NFeuD_CKKfkbkMVHQ/viewform?usp=sf_link', // @Tsu form URL goes here
-        initialSourceType: SourceType.URL,
-        onWebViewCreated: (controller) => webviewController = controller,
-      ),
-    );
+        height: MediaQuery.of(context).size.height - 64,
+        width: MediaQuery.of(context).size.width * 0.8,
+        color: Colors.white,
+        child: HtmlElementView(viewType: 'my_google_form'));
   }
 }
